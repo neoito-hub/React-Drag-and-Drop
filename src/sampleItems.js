78ebdea82011a6e8e8d6fef4d229e3a27d26034c
uberfreak <elitesuberfreak@csgo>
2020-01-20 20:37:36 +0530

    Initial Commit

---
+++ src/sampleItems.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    src:
+    "https://picsum.photos/id/1016/367/267",
+    id: "8c444080-9e02-4946-9205-ea65ed81a66f"
+  },
+  {
+    src:
+    "https://picsum.photos/id/1015/367/267",    id: "0a62f9fa-6f5c-4b09-8ff0-d2b9169426f9"
+  },
+  {
+    src:   "https://i.picsum.photos/id/10/2500/1667.jpg"
+    ,    id: "308f0373-a548-4802-8824-a1429ed3e801"
+  },
+   {
+   src:"https://cdn.wccftech.com/wp-content/uploads/2018/09/NVIDIA-GeForce-RTX.jpg",
+  id:"1"
+  },
+ 
+  {
+    src:"https://s3-ap-southeast-2.amazonaws.com/images.getjarvis.com.au/9068b2bd089c037a144fc847b9967ee83dce0fe5299261a884adc3241a33604b.jpeg"
+      ,
+    id: "2"
+  },
+  {
+    src:
+      "https://images.pexels.com/photos/1405773/pexels-photo-1405773.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    id: "3"
+  },
+  {
+    src:
+    "https://picsum.photos/id/100/367/267",
+    id: "4"
+  },
+  {
+    src:
+    "https://picsum.photos/id/1002/367/267",
+    id: "5"
+  },
+  {
+    src:"https://picsum.photos/id/1019/367/267",
+    id:"6"
+  }
+];

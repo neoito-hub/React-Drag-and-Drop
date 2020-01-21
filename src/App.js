eab2245d3eccb5b15f62523b77803cdf116d399a
uberfreak <elitesuberfreak@csgo>
2020-01-21 10:39:14 +0530

    Added responsiveness to the webpage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useContext } from "react";
 import DragItem from "./DragItem";
 import { Grid, GridImage, GridItem } from "./Grid";
 import GridContext from "./GridContext";
+import "./mainflex.css"
 
 function App() {
   const { items, moveItem } = useContext(GridContext);
 
   return (
-    <div className="App">
+    <div className="mainflex">
       <Grid>
         {items.map(item => (
           <DragItem key={item.id} id={item.id} onMoveItem={moveItem}>
